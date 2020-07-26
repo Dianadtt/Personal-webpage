d281176856005b3e3470414f51ffe2e8f8b2b208
Diana Toth <dianatoth.dtt@gmail.com>
2020-07-26 23:22:48 +0300

    Updates

---
+++ precache-manifest.c3a626997b6af5c2ed65458c68bc9dad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e31a4f3fb6e8d3b3d93c75a6fdcd5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edce355e5bda8d53d803",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4c382f282da2a7d1338e",
+    "url": "/static/css/main.3f462b8a.chunk.css"
+  },
+  {
+    "revision": "edce355e5bda8d53d803",
+    "url": "/static/js/2.ab2900bb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.ab2900bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c382f282da2a7d1338e",
+    "url": "/static/js/main.a8cd4b62.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e89c521ab0bc3fc9874ad0fbb2a555c4",
+    "url": "/static/media/aboutme.e89c521a.jpg"
+  },
+  {
+    "revision": "ea8bb1c516adbc195a2c94c3993912d7",
+    "url": "/static/media/avatar.ea8bb1c5.jpg"
+  },
+  {
+    "revision": "60a91004b4b37d810062f221d6e4459a",
+    "url": "/static/media/background.60a91004.jpg"
+  },
+  {
+    "revision": "f0f22b2949ce20459c5c403496043094",
+    "url": "/static/media/education.f0f22b29.jpg"
+  },
+  {
+    "revision": "65adfd9649f2285fa6e1e99bc839d516",
+    "url": "/static/media/file.65adfd96.pdf"
+  },
+  {
+    "revision": "182051237563aaf4c08669f4c43c7234",
+    "url": "/static/media/language.18205123.jpg"
+  },
+  {
+    "revision": "ef031c06616258b4e615e70bc4e530bd",
+    "url": "/static/media/me.ef031c06.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Personal-webpage/precache-manifest.95536ab165712ca6dd3fc381c8764e41.js"
+  "/precache-manifest.c3a626997b6af5c2ed65458c68bc9dad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Personal-webpage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
