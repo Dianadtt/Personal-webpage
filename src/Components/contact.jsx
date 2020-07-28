2b27288c4686a6207b3a398cab7c686591a665b3
Diana Toth <dianatoth.dtt@gmail.com>
2020-07-28 21:56:17 +0300

    card animation on hover

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,59 +1,44 @@
 import React, { Component } from 'react';
-// import { Grid, Cell } from 'react-mdl';
+
 import img from '../assets/avatar.jpg';
 import cv from '../assets/file.pdf'
 import { List, ListItem, ListItemContent } from 'react-mdl'
 import Form from './form'
-
+import Card from "./card"
 class Contact extends Component {
     render() {
         return (
             <>
-            <div className="card" id="contact-body">
-                {/* <Grid className="contact-grid">
-                    <Cell coll={6}> */}
-                <div id="contact-left">
-                    <h2>Toth Diana</h2>
-                    <img src={img} alt="diana toth" />
-                    <button>
-                        <a href={cv} download>Click to Download</a>
-                    </button>
+                <div className="card" id="contact-body">
 
-                </div>
-
-                {/* </Cell>
-                    <Cell coll={6}> */}
-
-
-                <div id="contact-right">
-                    <h2>Contact Me</h2>
-                    <List>
-                        <ListItem >
-                            <ListItemContent>
-                                <i className="fa fa-phone-square"></i>
+                    <div id="contact-left">
+                        <h2>Toth Diana</h2>
+                        <img src={img} alt="diana toth" />
+                        <button>
+                            <a href={cv} download>Click to Download</a>
+                        </button>
+                    </div>
+                    <div id="contact-right">
+                        <h2>Contact Me</h2>
+                        <List>
+                            <ListItem >
+                                <ListItemContent>
+                                    <i className="fa fa-phone-square"></i>
                                     0745 951 222
                                     </ListItemContent>
-                        </ListItem>
-                        <ListItem >
-                            <ListItemContent >
-                                <i className="fa fa-envelope"></i>
+                            </ListItem>
+                            <ListItem >
+                                <ListItemContent >
+                                    <i className="fa fa-envelope"></i>
                                      dianatoth.dtt@gmail.com
                                     </ListItemContent>
-                        </ListItem>
-                      
-
-
-                    </List>
-
+                            </ListItem>
+                        </List>
+                    </div>
                 </div>
-
-                {/* </Cell>
-                </Grid> */}
-               
-            </div>
-            <div className="card">
-                 <Form></Form>
-            </div>
+                <div className="card">
+                    <Form></Form>
+                </div>
             </>
         )
     }
